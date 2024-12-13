451496f7f1cdad4048dc387f7705cc5079146ebc
Joao Barbosa <joao.barbosa@ilegra.com>
2024-12-12 23:01:18 -0300

    add route public

--- private_subnets.tf
+++ private_subnets.tf
@@ -22,3 +22,30 @@ resource "aws_subnet" "private_subnet_1c" {
      Name= format("%s-private-subnet-1c", var.project_name)
    }
 }
+resource "aws_route_table" "private_internet_access" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = format("%s-private-1a", var.project_name)
+  }
+}
+
+resource "aws_route" "private_access" {
+  route_table_id         = aws_route_table.private_internet_access_1a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.nat_1a.id
+}
+
+resource "aws_route_table_association" "private_1a" {
+  subnet_id      = aws_subnet.private_subnet_1a.id
+  route_table_id = aws_route_table.private_internet_access.id
+}
+
+resource "aws_route_table_association" "private_1b" {
+  subnet_id      = aws_subnet.private_subnet_1b.id
+  route_table_id = aws_route_table.private_internet_access.id
+}
+
+resource "aws_route_table_association" "private_1c" {
+  subnet_id      = aws_subnet.private_subnet_1c.id
+  route_table_id = aws_route_table.private_internet_access.id
+}
